323c177183a4eff3bc3d516beaa1ea7e1f06fb5a
Mix3 <maksian431@gmail.com>
2025-12-06 16:05:41 +0100

    Fixed data_per_sector

--- ProjectCore/data_per_sector.py
+++ ProjectCore/data_per_sector.py
@@ -1,34 +1,37 @@
 import pandas as pd
 import re
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
+def createDataPerSector(wskazniki):
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
 
-df = pd.read_csv(
-    "Dane/wsk_fin.xlsx - dane.csv",
-    encoding="utf-8",
-    dtype={"numer PKD": str}
-)
+    df = pd.read_csv(
+        "Dane/wsk_fin.xlsx - dane.csv",
+        encoding="utf-8",
+        dtype={"numer PKD": str}
+    )
 
-df.columns = df.columns.str.strip()
-df["wskaźnik"] = df["wskaźnik"].str.strip()
-df["nazwa PKD"] = df["nazwa PKD"].str.strip()
-df["numer PKD"] = df["numer PKD"].str.strip()
+    df.columns = df.columns.str.strip()
+    df["wskaźnik"] = df["wskaźnik"].str.strip()
+    df["nazwa PKD"] = df["nazwa PKD"].str.strip()
+    df["numer PKD"] = df["numer PKD"].str.strip()
 
-wybrane_wskazniki = ["EN Liczba jednostek gospodarczych"] # USTALAMY WSKAZNIKI!!
+    wybrane_wskazniki = wskazniki# USTALAMY WSKAZNIKI!!
 
-filtrowane_dane = df[df["wskaźnik"].isin(wybrane_wskazniki)]
+    filtrowane_dane = df[df["wskaźnik"].isin(wybrane_wskazniki)]
 
-kolumny_lata = [str(rok) for rok in range(2005, 2025)]
-kolumny_do_wyswietlenia = ["numer PKD", "nazwa PKD", "wskaźnik"] + kolumny_lata
-wynik = filtrowane_dane[kolumny_do_wyswietlenia]
+    kolumny_lata = [str(rok) for rok in range(2005, 2025)]
+    kolumny_do_wyswietlenia = ["numer PKD", "nazwa PKD", "wskaźnik"] + kolumny_lata
+    wynik = filtrowane_dane[kolumny_do_wyswietlenia]
 
-wynik = wynik[wynik["numer PKD"].str.contains(r"[A-Za-z_ĄąĆćĘęŁłŃńÓóŚśŹźŻż]")]
+    wynik = wynik[wynik["numer PKD"].str.contains(r"[A-Za-z_ĄąĆćĘęŁłŃńÓóŚśŹźŻż]")]
 
-wynik = wynik.sort_values(by="numer PKD")
+    wynik = wynik.sort_values(by="numer PKD")
 
-print(wynik)
+    print(wynik)
 
-wynik.to_csv("Dane/dane_wedlug_wskaznikow.csv", index=False, encoding="utf-8")
+    wynik.to_csv("Dane/dane_wedlug_wskaznikow.csv", index=False, encoding="utf-8")
+
+createDataPerSector(["EN Liczba jednostek gospodarczych", "NP Wynik finansowy netto (zysk netto)"])
