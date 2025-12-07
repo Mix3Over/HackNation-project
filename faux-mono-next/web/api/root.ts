2b276a1f2fda8f432de67fad83188c97be777b04
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2025-12-07 10:49:13 +0100

    reload

--- faux-mono-next/web/api/root.ts
+++
@@ -1,7 +0,0 @@
-import { echoHandler } from "./routers/getting";
-import { testLoader } from "./test-loader/router";
-
-export const router = {
-	echoHandler,
-	testLoader,
-};
--- faux-mono-next/web/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useQuery } from "@tanstack/react-query";
-import { orpc } from "@/lib/orpc";
-
-function TestPage() {
-	const { data } = useQuery(orpc.testLoader.queryOptions());
-	return <pre>{JSON.stringify(data ?? {}).replaceAll("},", "},\n")}</pre>;
-}
-export default TestPage;
